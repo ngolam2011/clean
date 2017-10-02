75892036c8cfd29d3571cae07f55042b011ef636
Ngo Lam <lngo9@csc.com>
2017-10-02 15:34:14 +0700

    Boolean only

--- src/main/java/com/ngolam/cleancode/App.java
+++ src/main/java/com/ngolam/cleancode/App.java
@@ -10,8 +10,5 @@ public class App
     {
         Args arg = new Args("l,p#,d*", args);
 		boolean logging = arg.getBoolean('l');
-		int port = arg.getInt('p');
-		String directory = arg.getString('d');
-		System.out.println(logging + " " + port + " " + directory);
     }
 }
